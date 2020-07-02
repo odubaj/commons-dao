00dd4d19409e69b9772ab4354973bfc1abb5d1e2
Ivan Budayeu <ivan_budayeu@epam.com>
2020-07-02 11:24:47 +0300

    Thread sleep provided

--- src/main/java/com/epam/ta/reportportal/binary/CreateLogAttachmentService.java
+++ src/main/java/com/epam/ta/reportportal/binary/CreateLogAttachmentService.java
@@ -25,6 +25,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author <a href="mailto:ihar_kahadouski@epam.com">Ihar Kahadouski</a>
  */
@@ -43,5 +45,10 @@ public class CreateLogAttachmentService {
 		Log log = logRepository.findById(logId).orElseThrow(() -> new ReportPortalException(ErrorType.LOG_NOT_FOUND, logId));
 		log.setAttachment(attachment);
 		logRepository.save(log);
+		try {
+			TimeUnit.MINUTES.sleep(5);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 }
